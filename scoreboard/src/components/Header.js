3e1aa297c56b0b2d60bc4bb82cef42ae1dd51377
MichalDziedzic <michaldziedzic8@gmail.com>
2021-03-31 19:09:20 +0200

    test showing commit in profile

--- scoreboard/src/components/Header.js
+++ scoreboard/src/components/Header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Stats from './Stats';
 
 const Header = (props) => {
+  console.log('test commit');
     return (
       <header>
         <Stats players={props.players} />
         <h1>{ props.title }</h1>
-      </header>
+      </header> 
     );
   }
 
