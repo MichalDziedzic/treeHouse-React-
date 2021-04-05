f31fe55393a2a757ca8bd63ffbf5505e0473fa66
MichalDziedzic <michaldziedzic8@gmail.com>
2021-04-05 21:54:23 +0200

    init practise stars project

---
+++ practice-state/src/components/StarRating.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Star from './Star';
+
+class StarRating extends Component {
+
+  // Initialize a 'rating' state
+
+
+  // Write a function that returns 5 Star components
+
+
+  // Write an event handler that updates the rating state.
+  // Pass the function to a Star component via props
+
+
+  render() {
+    return (
+      <ul className="course--stars">
+        {/* Render the Star components */}
+        <Star/>
+      </ul>
+    );
+  }
+}
+
+export def
