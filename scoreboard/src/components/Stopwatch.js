ac902c8369f448c784b3b3bbf0bfd13ae13c0848
MichalDziedzic <michaldziedzic8@gmail.com>
2021-04-06 22:25:06 +0200

    update stopwatch

--- scoreboard/src/components/Stopwatch.js
+++ scoreboard/src/components/Stopwatch.js
@@ -2,13 +2,44 @@ import React, {Component} from 'react';
 
 class Stopwatch extends Component {
 
+    state={
+        isRunning:false,
+        elapsedTime:0,
+        previousTime:0,
+    }
+    handleStopwatch=()=>
+    {
+        console.log(this.state.isRunning)
+        this.setState((prevState)=>({isRunning:!prevState.isRunning}));
+        console.log(this.state.isRunning)
+        if(!this.setState.isRunning)
+            this.setState({previousTime:Date.now()})
+    }
+    componentDidMount()
+    {
+        this.intervalID=setInterval(() =>this.tick(),100);
+    }
+    tick = () => 
+    {
+        console.log('test');
+       if(this.state.isRunning)
+       {
+           const now=Date.now();
+           this.setState(prevState=>({
+               previousTime:now,
+               elapsedTime:prevState.elapsedTime + (now - this.state.previousTime)
+           }))
+       }
+    }
     render()
     {
         return(
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
-                <button>Start</button> 
+                <span className="stopwatch-time">{Math.floor(this.state.elapsedTime / 1000)}</span>
+                <button onClick={this.handleStopwatch}>
+                { this.state.isRunning ? 'Stop' : 'Start'}
+                </button> 
                 <button>Reset</button>
             </div>
 
