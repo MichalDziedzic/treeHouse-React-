200a36bb7c441d72191047fd935da51e4e755bb7
MichalDziedzic <michaldziedzic8@gmail.com>
2021-04-08 14:42:40 +0200

    update Player Form created Ref

--- scoreboard/src/components/AddPlayerForm.js
+++ scoreboard/src/components/AddPlayerForm.js
@@ -2,31 +2,25 @@ import React,{Component} from 'react';
 
 class AddPlayerForm extends Component {
 
-    state={
-        value:""
-    }
-handleValueChange=(e)=>
-{ 
-   
-    this.setState({value:e.target.value})
-} 
+  playerInput = React.createRef();
+
+
 handleSubmit=(e)=>
 {
     e.preventDefault();
-    this.props.addPlayer(this.state.value);
-    this.setState({value:''});
+    this.props.addPlayer(this.playerInput.current.value);
+    e.currentTarget.reset();
+    
 }
 render()
 { 
+    
     return(
         <form onSubmit={this.handleSubmit}>
             <input 
                 type="text"
-                value={this.state.value}
-                name="" 
-                id=""
                 placeholder="Enter a player's name"
-                onChange={this.handleValueChange}
+                ref={this.playerInput}
              />
              <input
                 type="submit"
