ffb4376b43d1e6c59dd9516f855928abb135062d
MichalDziedzic <michaldziedzic8@gmail.com>
2021-06-07 14:06:19 +0200

    change path URL by form

--- course-directory/src/components/Home.js
+++ course-directory/src/components/Home.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
 class Home extends Component {    
-  
+
+   handleSubmit=(e)=>
+    {
+      e.preventDefault();
+      let teacherName = this.name.value;
+      let teacherTopic = this.topic.value;
+      let path =`teachers/${teacherTopic}/${teacherName}`;
+      this.props.history.push(path);
+      
+    }
   render() {
+    
+   
     return (
+      
       <div className="main-content home">
         <h2>Front End Course Directory</h2>
         <p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
@@ -11,7 +23,12 @@ class Home extends Component {
         <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
         <hr />
         <h3>Teachers Featured</h3>
-        <Link to="teachers/HTML/Mike-Johnson">Mike Dallas</Link>
+        {/* <Link to="teachers/HTML/Mike-Johnson">Mike Dallas</Link> */}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Name" ref={(input)=>this.name = input}/>
+          <input type="text" placeholder="Topic" ref={(input)=>this.topic = input}/>
+          <button type="submit">Go!</button>
+      </form>
       </div>
     );
   }
